9edbccd818a93b58de886a19df819744de73434f
ori tikozki <oritikozki@gmail.com>
2022-06-27 17:51:59 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function StudentComp(){
+
+  let list = StudentsListComp();
+  return(
+    <>
+    {
+      list.map(s => 
+      <p style={s.grade <= 56 ? {backgroundColor: "red"}
+       : s.grade >= 57 && s.grade <= 75 ? {backgroundColor: 'yellow'}
+       : s.grade > 75 && s.grade <= 85 ? {backgroundColor: 'orange'} :
+       {backgroundColor: 'green'}} > {`${s.id} - ${s.name} - ${s.grade} - Teacher Words: 
+        ${s.grade <= 56 ? "Poor"
+       : s.grade >= 57 && s.grade <= 75 ? "OK"
+       : s.grade > 75 && s.grade <= 85 ? "Good" :
+       "Exellent" }`}
+       </p>)
+    }
+
+    </>
+  );
+}
+function StudentsListComp(){
+  let studentList = [
+  {id:1,name:"or shani",grade:100},
+  {id:2,name:"or lo shani",grade:10},
+  {id:3,name:"shmulik",grade:57},
+  {id:4,name:"Mishu",grade:76},
+  {id:5,name:"ze ah",grade:76},
+  {id:6,name:"ze hamor",grade:100},
+
+  ];
+  return studentList;
+}
+
+
+function GetYoutubeList(){
+  let list = [{title: "Onepiece video", url: "https://www.youtube.com/embed/SIyS3CSPXUg" ,width: 250, height: 400},
+  {title: "LOL Bel'veth", url: "https://www.youtube.com/embed/Vk2qDhvWhgI&t=641s" ,width: 250, height: 400},
+  {title: "LOL Bel'veth 2", url: "https://www.youtube.com/embed/W-ZAGWppvlw" ,width: 250, height: 400},
+]
+  return list;
+}
+
+function YouTubeVideoListComp(){
+  let objects = GetYoutubeList();
+    return(
+        objects.map(o => 
+        <YouTubeVideoComp width={o.width} height={o.height} url={o.url} title={o.title} ></YouTubeVideoComp>
+        )    
+    )
+  
+}
+
+function YouTubeVideoComp(props){
+ return (
+  <>
+  <h3> {props.title}</h3>
+  <iframe width={props.width} height={props.height} src={props.url} title={props.title} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  </>
+ )
+}
+
+
 root.render(
   <React.StrictMode>
-    <App />
+    {<StudentComp /> }
+    < YouTubeVideoListComp/>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
